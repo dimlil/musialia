92252db2d23f9bd7879a68dec4ecafa83fdf9e37
DimVasDev <d.v.vassilev@gmail.com>
2024-02-09 00:19:05 +0200

    feat/ added routes file

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = new Router();
+
+router.get('/', async(req, res) => {
+
+    try {
+        res.render('home');
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+export { router as indexRoute }
