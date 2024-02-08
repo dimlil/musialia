5bbe5d1f94f665ef5eaf33a55ad51bb93cb35be3
DimVasDev <d.v.vassilev@gmail.com>
2024-02-09 00:18:40 +0200

    feat/ added and set handlebars engine

--- config/express.js
+++ config/express.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import { engine as handlebars } from "express-handlebars"
+import { indexRoute } from '../routes/index.js';
 export default function setupExpress(app) {
     // app.use(express.urlencoded({ extended: true})); 
 
+    //Setup the view engine
+    app.engine('.hbs', handlebars({
+        extname: '.hbs'
+    }));
+    app.set('view engine', '.hbs'); 
+    // app.engine('.hbs', engine());
+    // app.set('view engine', 'handlebars');
+    // app.set('views', './views');
+
     //Setup the static files
-    app.use('/static', express.static('static'))
+    app.use('/static', express.static('static'));
+
+    app.use('/', indexRoute);
 
 };
