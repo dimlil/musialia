5e26fa9ff49e50aa7c7a8f67294bf14d4b028246
DimVasDev <d.v.vassilev@gmail.com>
2024-02-12 22:04:10 +0200

    feat/ added mongoDB conection

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { engine as handlebars } from "express-handlebars"
 import { indexRoute } from '../routes/index.js';
+import mongoose from 'mongoose';
 export default function setupExpress(app) {
     // app.use(express.urlencoded({ extended: true})); 
 
@@ -13,6 +14,14 @@ export default function setupExpress(app) {
     //Setup the static files
     app.use('/statics', express.static('static'));
 
+    // Mongo DB Connections
+    mongoose.connect(process.env.MONGO_DB_URL).then(response=>{
+        console.log('MongoDB Connection Succeeded.')
+    }).catch(error=>{
+        console.log('Error in DB connection: ' + error)
+    });
+
+    //Setup the routes
     app.use('/', indexRoute);
 
 };
