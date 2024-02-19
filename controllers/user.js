39c719ef052f982eddb69fbe97d5eb5d2425fd17
DimVasDev <d.v.vassilev@gmail.com>
2024-02-19 22:11:43 +0200

    feat/ create controler for registering user

---
+++ controllers/user.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import userSchema from '../models/user.js';
+
+export const registerUser = async (req,res)=>{
+    let {email, password} = req.body;
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+    const user = new userSchema({
+        email,
+        password,
+        role: 'user'
+    })
+    try {
+       await user.save();
+    } catch (error) {
+        
+    }
+}
